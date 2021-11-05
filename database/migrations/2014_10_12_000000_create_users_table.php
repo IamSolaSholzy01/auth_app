db1c4f9207161aa59adc4d737fbfa9419af59d36
IamSolaSholzy01 <65189840+IamSolaSholzy01@users.noreply.github.com>
2021-11-05 20:58:20 +0100

    Initial setup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->date('dob');
+            $table->string('username');
+            $table->enum('gender', ['male', 'female', 'other']);
             $table->rememberToken();
             $table->timestamps();
         });
